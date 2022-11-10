948585390f2df974a4d929623b5610cf3f0d77af
Juan Jesús Ortiz Vazquez <juanjesusov@gmail.com>
2022-11-10 17:38:39 -0600

    Add files via upload

---
+++ 1.1-addingNumbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1, num2, sum;
+
+    cout<<"Enter two numbers separated by a space:"<<endl;
+    cin>>num1>>num2;
+
+    sum = num1 + num2;
+
+    cout << num1<<" + "<<num2<<" = "<<sum<<endl;
+
+    return 0;
+}
